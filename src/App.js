15f624711a9c614638324111ecfff1958e3a1cb3
kazuki-kimura-6781 <kazukiforwork.bronze@gmail.com>
2024-09-27 05:10:40 +0000

    stateのリフトアップを行いました。

--- src/App.js
+++ src/App.js
@@ -1,41 +1,32 @@
 import { useState } from "react"; //ReactのuserState関数をインポート
 
-function Square() {
-  //Squareコンポーネントのvalueプロパティを削除
-  const [value, setValue] = useState(null); //現在のstate(value)とそれを更新する関数(setValue)が返されるようにし、useStateを呼び出す。useStateに渡されるnullはこのstate変数の初期値を表す。
-
-  function handleClick() {
-    setValue("X"); //SquareをクリックするとXを表示する
-  }
-
-  return (
-    <button className="square" onClick={handleClick}>
-      {value}
-    </button>
-  );
+function Square({ value }) {
+  return <button className="square">{value}</button>;
+  //Squareコンポーネントがvalue wo  stateで管理していた部分、及びonClickプロパティを削除
 }
 
 export default function Board() {
   //関数を定義し、exportにより外部からこのファイルにアクセスできるようになる、defaultにより、他のファイルはこの関数がメイン関数であると理解できる
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  //squaresというstate変数を定義し、デフォルト値として九つのマスに対応する九個のnullを持つ配列を与える。
   return (
     //()内の記述を関数の呼び出しもとに戻す
     <>
       <div className="board-row">
-        <Square />
-        {/*Squareコンポーネントがpropsを受け取らなくなったため、Boardコンポーネントが作成した9個のSquareコンポーネントからvalueプロパティを削除する*/}
-        <Square />
-        {/*各Squareはそれぞれ独自のstateを保持しており、それぞれのvalueはほかのものから完全に独立しているため、コンポーネントのset関数を呼び出すとReactは自動的に内部の子コンポーネントも更新する*/}
-        <Square />
+        <Square value={squares[0]} />
+        {/*レンダーする各Squareにpropsとしてvalueを渡していく*/}
+        <Square value={squares[1]} />
+        <Square value={squares[2]} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[3]} />
+        <Square value={squares[4]} />
+        <Square value={squares[5]} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[6]} />
+        <Square value={squares[7]} />
+        <Square value={squares[8]} />
       </div>
     </>
   );
